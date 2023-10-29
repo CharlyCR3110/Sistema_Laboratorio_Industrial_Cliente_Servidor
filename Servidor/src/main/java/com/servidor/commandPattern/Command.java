509f9b84931360bd9f9baa5f1350365b56b54f3f
charly <carlosgarita3110@gmail.com>
2023-10-29 14:51:56 -0600

    feat(commandPattern): add Command interface for command pattern implementation

--- Servidor/src/main/java/com/servidor/commandPattern/Command.java
+++ Servidor/src/main/java/com/servidor/commandPattern/Command.java
@@ -1,5 +1,10 @@
 package com.servidor.commandPattern;
 
-public interface Command {
+import java.util.List;
+
+public interface Command<T> {
+	public void setDatos(Object datos);
 	public int execute();
+	public T getReturn();
+	public List<T> getListReturn();
 }
