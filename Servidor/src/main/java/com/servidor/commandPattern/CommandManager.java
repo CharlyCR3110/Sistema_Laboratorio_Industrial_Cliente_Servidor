02d03fec23b604a85045d907c442fad9ab90f838
charly <carlosgarita3110@gmail.com>
2023-10-30 13:59:45 -0600

    feat(command-manager): update CommandManager to include Instrumento commands

--- Servidor/src/main/java/com/servidor/commandPattern/CommandManager.java
+++ Servidor/src/main/java/com/servidor/commandPattern/CommandManager.java
@@ -1,6 +1,7 @@
 package com.servidor.commandPattern;
 
 import com.servidor.commandPattern.TipoInstumento.*;
+import com.servidor.commandPattern.Instrumento.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +18,13 @@ public class CommandManager {
 		commandMap.put("LISTAR_TIPO_INSTRUMENTO", new ListarTipoInstrumentoCommand());
 		commandMap.put("MODIFICAR_TIPO_INSTRUMENTO", new ModificarTipoInstrumentoCommand());
 		commandMap.put("OBTENER_TIPO_INSTRUMENTO", new ObtenerTipoInstrumentoCommand());
+
+		// Commands para Instrumento
+		commandMap.put("GUARDAR_INSTRUMENTO", new GuardarInstrumentoCommand());
+		commandMap.put("ELIMINAR_INSTRUMENTO", new EliminarInstrumentoCommand());
+		commandMap.put("LISTAR_INSTRUMENTO", new ListarInstrumentoCommand());
+		commandMap.put("MODIFICAR_INSTRUMENTO", new ModificarInstrumentoCommand());
+		commandMap.put("OBTENER_INSTRUMENTO", new ObtenerInstrumentoCommand());
 
 		//DEBUG
 		System.out.println("CommandManager: initialized");
