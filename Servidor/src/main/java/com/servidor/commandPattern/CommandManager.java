58e30b9e951e3ba14a31607811d9ed1c1320081b
charly <carlosgarita3110@gmail.com>
2023-11-01 17:24:28 -0600

    refactor: refactor command initialization in CommandManager

--- Servidor/src/main/java/com/servidor/commandPattern/CommandManager.java
+++ Servidor/src/main/java/com/servidor/commandPattern/CommandManager.java
@@ -7,24 +7,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CommandManager {
+
+	// Atributos finales estáticos para los comandos
+	private final static GuardarTipoInstrumentoCommand guardarTipoInstrumentoCommand = new GuardarTipoInstrumentoCommand();
+	private final static EliminarTipoInstrumentoCommand eliminarTipoInstrumentoCommand = new EliminarTipoInstrumentoCommand();
+	private final static ListarTipoInstrumentoCommand listarTipoInstrumentoCommand = new ListarTipoInstrumentoCommand();
+	private final static ModificarTipoInstrumentoCommand modificarTipoInstrumentoCommand = new ModificarTipoInstrumentoCommand();
+	private final static ObtenerTipoInstrumentoCommand obtenerTipoInstrumentoCommand = new ObtenerTipoInstrumentoCommand();
+
+	private final static GuardarInstrumentoCommand guardarInstrumentoCommand = new GuardarInstrumentoCommand();
+	private final static EliminarInstrumentoCommand eliminarInstrumentoCommand = new EliminarInstrumentoCommand();
+	private final static ListarInstrumentoCommand listarInstrumentoCommand = new ListarInstrumentoCommand();
+	private final static ModificarInstrumentoCommand modificarInstrumentoCommand = new ModificarInstrumentoCommand();
+	private final static ObtenerInstrumentoCommand obtenerInstrumentoCommand = new ObtenerInstrumentoCommand();
+
+	// Mapa de comandos
 	private final Map<String, Command<?>> commandMap;
 
 	public CommandManager() {
 		commandMap = new HashMap<>();
 
-		// Commands para TipoInstrumento
-		commandMap.put("GUARDAR_TIPO_INSTRUMENTO", new GuardarTipoInstrumentoCommand());
-		commandMap.put("ELIMINAR_TIPO_INSTRUMENTO", new EliminarTipoInstrumentoCommand());
-		commandMap.put("LISTAR_TIPO_INSTRUMENTO", new ListarTipoInstrumentoCommand());
-		commandMap.put("MODIFICAR_TIPO_INSTRUMENTO", new ModificarTipoInstrumentoCommand());
-		commandMap.put("OBTENER_TIPO_INSTRUMENTO", new ObtenerTipoInstrumentoCommand());
+		commandMap.put("GUARDAR_TIPO_INSTRUMENTO", guardarTipoInstrumentoCommand);
+		commandMap.put("ELIMINAR_TIPO_INSTRUMENTO", eliminarTipoInstrumentoCommand);
+		commandMap.put("LISTAR_TIPO_INSTRUMENTO", listarTipoInstrumentoCommand);
+		commandMap.put("MODIFICAR_TIPO_INSTRUMENTO", modificarTipoInstrumentoCommand);
+		commandMap.put("OBTENER_TIPO_INSTRUMENTO", obtenerTipoInstrumentoCommand);
 
-		// Commands para Instrumento
-		commandMap.put("GUARDAR_INSTRUMENTO", new GuardarInstrumentoCommand());
-		commandMap.put("ELIMINAR_INSTRUMENTO", new EliminarInstrumentoCommand());
-		commandMap.put("LISTAR_INSTRUMENTO", new ListarInstrumentoCommand());
-		commandMap.put("MODIFICAR_INSTRUMENTO", new ModificarInstrumentoCommand());
-		commandMap.put("OBTENER_INSTRUMENTO", new ObtenerInstrumentoCommand());
+		commandMap.put("GUARDAR_INSTRUMENTO", guardarInstrumentoCommand);
+		commandMap.put("ELIMINAR_INSTRUMENTO", eliminarInstrumentoCommand);
+		commandMap.put("LISTAR_INSTRUMENTO", listarInstrumentoCommand);
+		commandMap.put("MODIFICAR_INSTRUMENTO", modificarInstrumentoCommand);
+		commandMap.put("OBTENER_INSTRUMENTO", obtenerInstrumentoCommand);
 
 		//DEBUG
 		System.out.println("CommandManager: initialized");
