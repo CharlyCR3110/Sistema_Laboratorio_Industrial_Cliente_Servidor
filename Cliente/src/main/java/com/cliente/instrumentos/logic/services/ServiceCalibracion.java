feb8db9c88ae40430bb90f6f7d2f0de8551555e8
charly <carlosgarita3110@gmail.com>
2023-11-02 13:04:15 -0600

    feat: create ServiceCalibracion class
    Added the `ServiceCalibracion` class responsible for managing Calibraicion-related operations, including saving, deleting, modifying, listing, and obtaining data. This class uses the `ClienteServidorHandler` for client-server communication.

---
+++ Cliente/src/main/java/com/cliente/instrumentos/logic/services/ServiceCalibracion.java
@@ -0,0 +1,94 @@
+package com.cliente.instrumentos.logic.services;
+
+import com.cliente.instrumentos.logic.ClienteServidorHandler;
+import com.compartidos.elementosCompartidos.Calibracion;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ServiceCalibracion {
+	private final ClienteServidorHandler clienteServidorHandler = ClienteServidorHandler.instance();
+	private static ServiceCalibracion theInstance;
+	public static ServiceCalibracion instance(){
+		if (theInstance == null) theInstance = new ServiceCalibracion();
+		return theInstance;
+	}
+	private ServiceCalibracion(){
+	}
+	public ServiceCalibracion getInstance() {
+		return theInstance;
+	}
+
+	public int guardar(Calibracion calibracion) {
+		try {
+			ClienteServidorHandler.enviarComandoAlServidor(GUARDAR_CALIBRACION, calibracion);
+			return 1;
+		} catch (Exception e) {
+			// Manejar errores aquí
+			e.printStackTrace();
+			throw new RuntimeException("Error en ServiceInstrumentos.guardar: " + e.getMessage());
+		}
+	}
+
+	public int eliminar(Calibracion calibracion) {
+		try {
+			ClienteServidorHandler.enviarComandoAlServidor(ELIMINAR_CALIBRACION, calibracion);
+			return 1;
+		} catch (Exception e) {
+			// Manejar errores aquí
+			e.printStackTrace();
+			throw new RuntimeException("Error en ServiceInstrumentos.eliminar: " + e.getMessage());
+		}
+	}
+
+	public int modificar(Calibracion calibracion) {
+		try {
+			ClienteServidorHandler.enviarComandoAlServidor(MODIFICAR_CALIBRACION, calibracion);
+			return 1;
+		} catch (Exception e) {
+			// Manejar errores aquí
+			e.printStackTrace();
+			throw new RuntimeException("Error en ServiceInstrumentos.modificar: " + e.getMessage());
+		}
+	}
+
+	public List<Calibracion> listar(Calibracion filter) {
+		Object returnObject;
+		try {
+			returnObject = ClienteServidorHandler.enviarComandoAlServidor(LISTAR_CALIBRACION, filter);
+		} catch (Exception e) {
+			// Manejar errores aquí
+			e.printStackTrace();
+			throw new RuntimeException("Error en ServiceCalibracions.listar: " + e.getMessage());
+		}
+
+		if (returnObject instanceof List<?>) {
+			// Verificar si la lista contiene objetos del tipo Calibracion
+			List<?> lista = (List<?>) returnObject;
+			if (!lista.isEmpty() && lista.get(0) instanceof Calibracion) {
+				return (List<Calibracion>) returnObject;
+			}
+		}
+		// caso en el que el resultado no es del tipo esperado
+		// devolver una lista vacía de Calibracion
+		return Collections.emptyList(); // Devuelve una lista vacía en caso de error
+	}
+
+	public Calibracion obtener(Calibracion calibracion) {
+		try {
+			return (Calibracion) ClienteServidorHandler.enviarComandoAlServidor(OBTENER_CALIBRACION, calibracion);
+		} catch (Exception e) {
+			// Manejar errores aquí
+			e.printStackTrace();
+			throw new RuntimeException("Error en ServiceInstrumentos.obtener: " + e.getMessage());
+		}
+	}
+
+
+	// constantes para los comandos	(no hay motivo real para que sean static)
+	private static final String GUARDAR_CALIBRACION = "GUARDAR_CALIBRACION";
+	private static final String ELIMINAR_CALIBRACION = "ELIMINAR_CALIBRACION";
+	private static final String LISTAR_CALIBRACION = "LISTAR_CALIBRACION";
+	private static final String MODIFICAR_CALIBRACION = "MODIFICAR_CALIBRACION";
+	private static final String OBTENER_CALIBRACION = "OBTENER_CALIBRACION";
+}
