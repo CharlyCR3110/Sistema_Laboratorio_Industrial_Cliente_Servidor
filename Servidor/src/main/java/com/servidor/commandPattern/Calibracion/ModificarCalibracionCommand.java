ee5fa37c674c2c2142e4130996f5b334d006cc19
charly <carlosgarita3110@gmail.com>
2023-11-01 18:28:19 -0600

    feat(commandPattern): add ModificarCalibracionCommand class for command pattern

---
+++ Servidor/src/main/java/com/servidor/commandPattern/Calibracion/ModificarCalibracionCommand.java
@@ -0,0 +1,36 @@
+package com.servidor.commandPattern.Calibracion;
+
+import com.compartidos.elementosCompartidos.Calibracion;
+import com.servidor.commandPattern.Command;
+import com.servidor.instrumentos.dbRelated.controller.CalibracionDaoController;
+
+import java.util.List;
+public class ModificarCalibracionCommand implements Command<Calibracion> {
+	private final CalibracionDaoController calibracionDaoController = new CalibracionDaoController();
+	private Calibracion calibracion;
+	public ModificarCalibracionCommand(){}
+
+	@Override
+	public void setDatos(Object datos) {
+		this.calibracion = (Calibracion) datos;
+	}
+
+	@Override
+	public int execute() {
+		try {
+			return calibracionDaoController.modificar(calibracion);
+		} catch (Exception e) {
+			throw new RuntimeException("ERROR AL ELIMINAR INSTRUMENTO");
+		}
+	}
+
+	@Override
+	public Calibracion getReturn() {
+		return null;
+	}
+
+	@Override
+	public List<Calibracion> getListReturn() {
+		return null;
+	}
+}
