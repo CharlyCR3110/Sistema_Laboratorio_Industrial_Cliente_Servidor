310d9b3ca571675fc3d26010ea29d7c8807ae09e
charly <carlosgarita3110@gmail.com>
2023-11-02 01:02:35 -0600

    feat: create ServiceTipoInstrumentos class
    Added the `ServiceTipoInstrumentos` class responsible for managing TipoInstrumento-related operations, including saving, deleting, modifying, listing, and obtaining data. This class uses the `ClienteServidorHandler` for client-server communication.
    
    The class includes the following methods:
    - `guardar`: Save a TipoInstrumento.
    - `eliminar`: Delete a TipoInstrumento.
    - `modificar`: Modify a TipoInstrumento.
    - `listar`: List TipoInstrumentos based on a filter.
    - `obtener`: Obtain a TipoInstrumento.
    
    These methods handle exceptions and provide error messages in case of failures. Additionally, the class defines constants for command names.
    
    This class facilitates communication with the server for TipoInstrumento operations.

---
+++ Cliente/src/main/java/com/cliente/instrumentos/logic/services/ServiceTipoInstrumentos.java
@@ -0,0 +1,104 @@
+package com.cliente.instrumentos.logic.services;
+
+import com.cliente.instrumentos.logic.ClienteServidorHandler;
+import com.compartidos.elementosCompartidos.TipoInstrumento;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ServiceTipoInstrumentos {
+	private final ClienteServidorHandler clienteServidorHandler = ClienteServidorHandler.instance();
+	private static ServiceTipoInstrumentos theInstance;
+	public static ServiceTipoInstrumentos instance(){
+		if (theInstance == null) theInstance = new ServiceTipoInstrumentos();
+		return theInstance;
+	}
+	private ServiceTipoInstrumentos(){
+	}
+	public ServiceTipoInstrumentos getInstance() {
+		return theInstance;
+	}
+
+	public int guardar(TipoInstrumento tipoInstrumento) {
+		try {
+			ClienteServidorHandler.enviarComandoAlServidor(GUARDAR_TIPO_INSTRUMENTO, tipoInstrumento);
+			return 1;
+		} catch (Exception e) {
+			// Manejar errores aquí
+			e.printStackTrace();
+			throw new RuntimeException("Error en ServiceTipoInstrumentos.guardarTipoInstrumento: " + e.getMessage());
+		}
+	}
+
+	public int eliminar(TipoInstrumento tipoInstrumento) {
+		try {
+			ClienteServidorHandler.enviarComandoAlServidor(ELIMINAR_TIPO_INSTRUMENTO, tipoInstrumento);
+			return 1;
+		} catch (Exception e) {
+			// Manejar errores aquí
+			e.printStackTrace();
+			throw new RuntimeException("Error en ServiceTipoInstrumentos.eliminarTipoInstrumento: " + e.getMessage());
+		}
+	}
+
+	public int modificar(TipoInstrumento tipoInstrumento) {
+		try {
+			ClienteServidorHandler.enviarComandoAlServidor(MODIFICAR_TIPO_INSTRUMENTO, tipoInstrumento);
+			return 1;
+		} catch (Exception e) {
+			// Manejar errores aquí
+			e.printStackTrace();
+			throw new RuntimeException("Error en ServiceTipoInstrumentos.modificarTipoInstrumento: " + e.getMessage());
+		}
+	}
+
+	public List<TipoInstrumento> listar(TipoInstrumento filter) {
+		Object returnObject;
+		try {
+			returnObject = ClienteServidorHandler.enviarComandoAlServidor(LISTAR_TIPO_INSTRUMENTO, filter);
+		} catch (Exception e) {
+			// Manejar errores aquí
+			e.printStackTrace();
+			throw new RuntimeException("Error en ServiceTipoInstrumentos.listar: " + e.getMessage());
+		}
+
+		if (returnObject instanceof List<?>) {
+			// Verificar si la lista contiene objetos del tipo TipoInstrumento
+			List<?> lista = (List<?>) returnObject;
+			if (!lista.isEmpty() && lista.get(0) instanceof TipoInstrumento) {
+				return (List<TipoInstrumento>) returnObject;
+			}
+		}
+		// caso en el que el resultado no es del tipo esperado
+		// devolver una lista vacía de TipoInstrumento
+		return Collections.emptyList(); // Devuelve una lista vacía en caso de error
+	}
+
+		public TipoInstrumento obtener(TipoInstrumento tipoInstrumento) {
+		try {
+			return (TipoInstrumento) ClienteServidorHandler.enviarComandoAlServidor(OBTENER_TIPO_INSTRUMENTO, tipoInstrumento);
+		} catch (Exception e) {
+			// Manejar errores aquí
+			e.printStackTrace();
+			throw new RuntimeException("Error en ServiceTipoInstrumentos.obtener: " + e.getMessage());
+		}
+	}
+	public TipoInstrumento getTipoSeleccionado(String tipo) {
+		for (TipoInstrumento tipoInstrumento : listar(new TipoInstrumento())) {
+			if (tipoInstrumento.getNombre().equals(tipo)) {
+				return tipoInstrumento;
+			}
+		}
+		return null;
+	}
+
+
+
+
+	// constantes para los comandos
+	public static final String GUARDAR_TIPO_INSTRUMENTO = "GUARDAR_TIPO_INSTRUMENTO";
+	public static final String ELIMINAR_TIPO_INSTRUMENTO = "ELIMINAR_TIPO_INSTRUMENTO";
+	public static final String LISTAR_TIPO_INSTRUMENTO = "LISTAR_TIPO_INSTRUMENTO";
+	public static final String MODIFICAR_TIPO_INSTRUMENTO = "MODIFICAR_TIPO_INSTRUMENTO";
+	public static final String OBTENER_TIPO_INSTRUMENTO = "OBTENER_TIPO_INSTRUMENTO";
+}
