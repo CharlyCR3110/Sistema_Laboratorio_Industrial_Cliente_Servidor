42f8cfc2f45525542e148dd37b2ae3e469a3db7a
charly <carlosgarita3110@gmail.com>
2023-11-07 22:49:30 -0600

    Add thread management for client connections

--- Servidor/src/main/java/com/servidor/network/Server.java
+++ Servidor/src/main/java/com/servidor/network/Server.java
@@ -2,8 +2,11 @@ package com.servidor.network;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
+	private static List<Thread> threads = new ArrayList<>();
 	public static void main(String[] args) {
 		System.out.println("Hello world from Server!");
 		int port = 12345;
@@ -11,16 +14,20 @@ public class Server {
 		try (ServerSocket serverSocket = new ServerSocket(port)) {
 			System.out.println("Server is listening on port " + port);
 			while (true) {
+				System.out.println("Clientes conectados: " + threads.size());
 				// Espera a que llegue una conexión entrante
 				Socket socket = serverSocket.accept();
 				System.out.println("Cliente conectado desde " + socket.getInetAddress());
 
 				// Crea una nueva instancia de ClientHandler para manejar la conexión del cliente
-				ClientHandler clientHandler = new ClientHandler(socket);
+				ClientHandler clientHandler = new ClientHandler(socket, threads);
 
 				// Inicia un nuevo hilo para manejar la conexión del cliente
 				Thread thread = new Thread(clientHandler);
 				thread.start();
+
+				// Agrega el hilo a la lista de hilos
+				threads.add(thread);
 			}
 		} catch (Exception e) {
 			System.out.println("Server exception: " + e.getMessage());
