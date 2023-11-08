97a24e8c2cf88c8bc8a9a704a1f3b3b5ed191f3a
charly <carlosgarita3110@gmail.com>
2023-11-08 15:12:31 -0600

    refactor: refactor Server class to use a Map of client threads to ObjectOutputStreams (clientStreams) instead of a List of Threads

--- Servidor/src/main/java/com/servidor/network/Server.java
+++ Servidor/src/main/java/com/servidor/network/Server.java
@@ -1,12 +1,15 @@
 package com.servidor.network;
 
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Server {
-	private static List<Thread> threads = new ArrayList<>();
+	private static Map<Thread, ObjectOutputStream> clientStreams = new HashMap<>();
 	public static void main(String[] args) {
 		System.out.println("Hello world from Server!");
 		int port = 12345;
@@ -14,20 +17,20 @@ public class Server {
 		try (ServerSocket serverSocket = new ServerSocket(port)) {
 			System.out.println("Server is listening on port " + port);
 			while (true) {
-				System.out.println("Clientes conectados: " + threads.size());
+				System.out.println("Clientes conectados: " + clientStreams.size());
 				// Espera a que llegue una conexión entrante
 				Socket socket = serverSocket.accept();
 				System.out.println("Cliente conectado desde " + socket.getInetAddress());
 
 				// Crea una nueva instancia de ClientHandler para manejar la conexión del cliente
-				ClientHandler clientHandler = new ClientHandler(socket, threads);
+				ClientHandler clientHandler = new ClientHandler(socket, clientStreams);
 
 				// Inicia un nuevo hilo para manejar la conexión del cliente
 				Thread thread = new Thread(clientHandler);
 				thread.start();
 
 				// Agrega el hilo a la lista de hilos
-				threads.add(thread);
+				clientStreams.put(thread, clientHandler.getOutputSteam());
 			}
 		} catch (Exception e) {
 			System.out.println("Server exception: " + e.getMessage());
