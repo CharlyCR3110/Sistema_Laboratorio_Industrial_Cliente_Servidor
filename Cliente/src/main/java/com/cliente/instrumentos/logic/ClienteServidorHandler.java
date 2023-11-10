8da015e3b5b5e4a726cdc145dbdca48816d2c0d5
charly <carlosgarita3110@gmail.com>
2023-11-10 10:01:30 -0600

    chore: add async functionality to ClienteServidorHandler

--- Cliente/src/main/java/com/cliente/instrumentos/logic/ClienteServidorHandler.java
+++ Cliente/src/main/java/com/cliente/instrumentos/logic/ClienteServidorHandler.java
@@ -1,17 +1,28 @@
 package com.cliente.instrumentos.logic;
 
+import com.cliente.instrumentos.logic.async.IListener;
+import com.cliente.instrumentos.logic.async.ITarget;
+import com.compartidos.elementosCompartidos.MensajeAsincrono;
 import com.compartidos.elementosCompartidos.ObjectSocket;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-public class ClienteServidorHandler {
+public class ClienteServidorHandler implements IListener {
 	private static final String SERVER_ADDRESS = "localhost";
 	private static final int SERVER_PORT = 12345;
 	private static ClienteServidorHandler theInstance;
-	private static ObjectSocket objectSocketSync;	// socket Sync encargado de enviar y recibir mensajes
+	public ObjectSocket objectSocketSync;	// socket Sync encargado de enviar y recibir mensajes
+	public ObjectSocket objectSocketAsync;	// socket Async encargado de recibir notificaciones
+	public ITarget target;
+
 	public static synchronized ClienteServidorHandler instance() {
+		if (theInstance == null) theInstance = new ClienteServidorHandler();
+		return theInstance;
+	}
+
+	public static synchronized IListener instanceListener() {
 		if (theInstance == null) theInstance = new ClienteServidorHandler();
 		return theInstance;
 	}
@@ -22,8 +33,11 @@ public class ClienteServidorHandler {
 
 	private void connect(){
 		try {
-			Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
-			objectSocketSync = new ObjectSocket(socket);
+			objectSocketSync = new ObjectSocket(new Socket(SERVER_ADDRESS, SERVER_PORT));
+			objectSocketSync.out.writeInt(1);	// 1: Sincrono, 2: Asincrono
+			objectSocketSync.out.flush();
+			String sid = (String) objectSocketSync.in.readObject();
+			System.out.println("SYNCH: " + sid);
 		} catch (Exception e) {
 			System.out.println("Error al conectar el socketSync al servidor");
 			throw new RuntimeException(e);
@@ -37,6 +51,7 @@ public class ClienteServidorHandler {
 	// Método para enviar la solicitud al servidor
 	private synchronized void enviarSolicitudAlServidor(String commandName, Object datos) {
 		try {
+			System.out.println("Enviando solicitud al servidor: " + commandName);
 			objectSocketSync.out.writeObject(commandName);
 			objectSocketSync.out.writeObject(datos);
 			objectSocketSync.out.flush();
@@ -48,25 +63,11 @@ public class ClienteServidorHandler {
 	// Método para recibir la respuesta del servidor
 	private synchronized Object recibirRespuestaDelServidor() {
 		try {
+			System.out.println("Esperando respuesta del servidor...");
 			return objectSocketSync.in.readObject();
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-	}
-
-	// Método para manejar notificaciones en un hilo separado
-	public void manejarNotificaciones() {
-		new Thread(() -> {
-			try {
-				while (true) {
-					Object notificacion = recibirRespuestaDelServidor();
-					// Lógica para manejar la notificación (puedes adaptar según tus necesidades)
-					System.out.println("Notificación del servidor: " + notificacion);
-				}
-			} catch (Exception e) {
-				e.printStackTrace(); // Manejar la excepción adecuadamente
-			}
-		}).start();
 	}
 
 	// Método para enviar comandos al servidor y recibir respuestas
@@ -99,5 +100,20 @@ public class ClienteServidorHandler {
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	@Override
+	public void addTarget(ITarget t) {
+		this.target = t;
+	}
+
+	@Override
+	public void start() {
+
+	}
+
+	@Override
+	public void stop() {
+
 	}
 }
