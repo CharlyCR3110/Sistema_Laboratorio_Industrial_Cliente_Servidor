d13d9b2f3247dd939047a7c55e05d3247767c185
charly <carlosgarita3110@gmail.com>
2023-11-10 09:28:06 -0600

    chore: add notification socket to Server class

--- Servidor/src/main/java/com/servidor/network/Server.java
+++ Servidor/src/main/java/com/servidor/network/Server.java
@@ -1,20 +1,22 @@
 package com.servidor.network;
 
+import com.compartidos.elementosCompartidos.MensajeAsincrono;
 import com.compartidos.elementosCompartidos.ObjectSocket;
 
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
 
 public class Server {
 	ServerSocket serverSocket;
+	ServerSocket notificationSocket;
 	List<ClientHandler> workers;
 	private final int PORT = 12345;
 	public Server() {
 		try {
 			serverSocket = new ServerSocket(PORT);
+			notificationSocket = new ServerSocket(PORT + 1);
 			workers = Collections.synchronizedList(new ArrayList<ClientHandler>());
 			System.out.println("Servidor iniciado...");
 		} catch (IOException ex) {
@@ -26,13 +28,17 @@ public class Server {
 	public void run() {
 		boolean continuar = true;
 		ObjectSocket objectSocketSync = null;
+		ObjectSocket objectSocketAsync = null;
 		Socket socket = null;
+		Socket notificationSocket = null;
 		while (continuar) {
 			try {
 				socket = serverSocket.accept();
+				notificationSocket = this.notificationSocket.accept();
 				objectSocketSync = new ObjectSocket(socket);
+				objectSocketAsync = new ObjectSocket(notificationSocket);
 				System.out.println("Conexion Establecida...");
-				ClientHandler worker = new ClientHandler(this, objectSocketSync);
+				ClientHandler worker = new ClientHandler(this, objectSocketSync, objectSocketAsync);
 				workers.add(worker);
 				System.out.println("Quedan: " + workers.size());
 				worker.start();
@@ -55,5 +61,11 @@ public class Server {
 
 	public void remove(ClientHandler clientHandler) {
 		workers.remove(clientHandler);
+	}
+
+	public void deliver(MensajeAsincrono message) {
+		for (ClientHandler worker : workers) {
+			worker.deliver(message);
+		}
 	}
 }
