23f5749563a5ff2d4f9003dd406342109b3b23f5
charly <carlosgarita3110@gmail.com>
2023-11-12 14:49:27 -0600

    style: remove debug print statements and improve error handling in ClienteServidorHandler.java

--- Cliente/src/main/java/com/cliente/instrumentos/logic/ClienteServidorHandler.java
+++ Cliente/src/main/java/com/cliente/instrumentos/logic/ClienteServidorHandler.java
@@ -40,9 +40,7 @@ public class ClienteServidorHandler implements IListener {
 			objectSocketSync.out.writeInt(Protocol.SYNC);	// 1: Sincrono, 2: Asincrono
 			objectSocketSync.out.flush();
 			objectSocketSync.sid = (String) objectSocketSync.in.readObject();
-			System.out.println("SYNCH: " + objectSocketSync.sid);
 		} catch (Exception e) {
-			System.out.println("Error al conectar el socketSync al servidor");
 			throw new RuntimeException(e);
 		}
 	}
@@ -54,10 +52,6 @@ public class ClienteServidorHandler implements IListener {
 	// Método para enviar la solicitud al servidor
 	private synchronized void enviarSolicitudAlServidor(String commandName, Object datos) {
 		try {
-			System.out.println("---------kk----------");
-			System.out.println("SID: " + objectSocketSync.sid);    // DEBUG
-			System.out.println("Enviando solicitud al servidor: " + commandName);
-			System.out.println("Datos: " + datos);
 			// limpiar el buffer de salida
 			objectSocketSync.out.reset();
 			// enviar los datos
@@ -65,9 +59,6 @@ public class ClienteServidorHandler implements IListener {
 			objectSocketSync.out.writeObject(commandName);
 			objectSocketSync.out.writeObject(datos);
 			objectSocketSync.out.flush();
-			System.out.println("Solicitud enviada");
-			System.out.println("Datos enviados. Protocolo: " + Protocol.NORMAL + " CommandName: " + commandName + " Datos: " + datos);
-			System.out.println("---------kk-----------");
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
@@ -76,7 +67,6 @@ public class ClienteServidorHandler implements IListener {
 	// Método para recibir la respuesta del servidor
 	private synchronized Object recibirRespuestaDelServidor() {
 		try {
-			System.out.println("Esperando respuesta del servidor...");
 			return objectSocketSync.in.readObject();
 		} catch (Exception e) {
 			throw new RuntimeException(e);
@@ -119,7 +109,6 @@ public class ClienteServidorHandler implements IListener {
 	}
 
 	public void startListening() {
-		System.out.println("startListening");
 		try {
 			objectSocketAsync = new ObjectSocket(new Socket(Protocol.SERVER, Protocol.PORT));	// <- AQUI HAY PROBLEMAS
 			objectSocketAsync.sid = objectSocketSync.sid;
@@ -129,20 +118,16 @@ public class ClienteServidorHandler implements IListener {
 			objectSocketAsync.out.writeInt(Protocol.ASYNC);
 			objectSocketAsync.out.writeObject(objectSocketAsync.sid);
 			objectSocketAsync.out.flush();
-			System.out.println("ASYNCH: " + objectSocketAsync.sid);
 		} catch (SocketException e) {
-			System.out.println("SocketException - > startListening: Error al conectar el socketAsync al servidor. Motivo: " + e.getMessage());
+			throw new RuntimeException("Socket Exception: " + e.getMessage());
 		} catch (Exception e) {
-			System.out.println("startListening: Error al conectar el socketAsync al servidor");
 			throw new RuntimeException(e);
 		}
 
-		System.out.println("startListening: Creando hilo secundario");
 		// Se crea un hilo secundario para escuchar los mensajes asincronos
 		Thread t = new Thread(new Runnable() {
 			@Override
 			public void run() {
-				System.out.println("startListening: Escuchando...");
 				listen();
 			}
 		});
@@ -153,24 +138,19 @@ public class ClienteServidorHandler implements IListener {
 	public void listen() {
 		int method;
 		while (continuar) {
-			System.out.println("Escuchandoss...");
 			try {
 				method = objectSocketAsync.in.readInt();
-				System.out.println("Operacion: " + method);
 				switch (method) {
 					case Protocol.DELIVER:
 						try {
 							MensajeAsincrono mensajeAsincrono = (MensajeAsincrono) objectSocketAsync.in.readObject();
 							this.deliver(mensajeAsincrono);
-							System.out.println("Mensaje asincrono recibido: " + mensajeAsincrono.toString());
 						} catch (ClassNotFoundException ex) {
-							System.out.println("listen: Error al recibir el mensaje asincrono");
 							throw new RuntimeException(ex);
 						}
 						break;
 				}
 			} catch (Exception e) {
-				System.out.println("listen: Error: " + e.getMessage());
 				continuar = false;
 				throw new RuntimeException(e);
 			}
@@ -181,7 +161,6 @@ public class ClienteServidorHandler implements IListener {
 			objectSocketAsync.socket.shutdownOutput();
 			objectSocketAsync.socket.close();
 		} catch (Exception e) {
-			System.out.println("listen: Error al cerrar el socketAsync");
 			throw new RuntimeException(e);
 		}
 	}
@@ -202,7 +181,6 @@ public class ClienteServidorHandler implements IListener {
 		try {
 			this.disconnect();
 		} catch (Exception e) {
-			System.out.println("stop: Error al desconectar el socketSync");
 			throw new RuntimeException(e);
 		}
 	}
@@ -212,7 +190,6 @@ public class ClienteServidorHandler implements IListener {
 			objectSocketSync.socket.shutdownOutput();
 			objectSocketSync.socket.close();
 		} catch (Exception e) {
-			System.out.println("disconnect: Error al desconectar el socketSync");
 			throw new RuntimeException(e);
 		}
 	}
