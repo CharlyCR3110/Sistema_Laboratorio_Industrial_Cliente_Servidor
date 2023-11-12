2b69b66b2181cd2589bedc6feee1e7a4f5be7dd7
charly <carlosgarita3110@gmail.com>
2023-11-11 21:28:23 -0600

    refactor: update View form and Java class to display notifications table and add clear button

--- Cliente/src/main/java/com/cliente/instrumentos/presentation/notificaciones/View.java
+++ Cliente/src/main/java/com/cliente/instrumentos/presentation/notificaciones/View.java
@@ -1,22 +1,52 @@
 package com.cliente.instrumentos.presentation.notificaciones;
 
 import javax.swing.*;
+import javax.swing.table.TableColumnModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Observable;
+import java.util.Observer;
 
-public class View {
+public class View implements Observer {
 	private JTextPane registro;
 	private JPanel panel;
-	private JLabel title;
-
+	private JButton clean;
+	private JTable notificacionesTable;
+	private Controller controller;
 	public View() {
-		registro.setEditable(false);
-	}
-
-	public void agregarMensaje(String mensaje) {
-		registro.setText(registro.getText() + "\n" + mensaje);
+		clean.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					controller.clear();
+				} catch (Exception ex) {
+					System.out.println(ex.getMessage());
+				}
+			}
+		});
 	}
 
 	public JPanel getPanel() {
 		return panel;
 	}
 
+	public void setController(Controller controller) {
+		this.controller = controller;
+	}
+
+	@Override
+	public void update(Observable updatedModel, Object properties) {
+		int props = (int) properties;
+		if ((props & Model.LIST) == Model.LIST) {
+			int[] cols = {TableModel.MENSAJE};
+			notificacionesTable.setModel(new TableModel(cols, controller.getModel().getList()));
+			notificacionesTable.setRowHeight(40);
+			notificacionesTable.setIntercellSpacing(new Dimension(10, 10));
+			notificacionesTable.setShowGrid(false);
+			TableColumnModel columnModel = notificacionesTable.getColumnModel();
+			columnModel.getColumn(0).setPreferredWidth(100);
+		}
+		this.panel.revalidate();
+	}
 }
